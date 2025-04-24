e887f894d4479d8c4b942b781d4c103fcc58cdfe
CristianTorresCarrero} <cristiantorrescarrero@gmail.com>
2025-04-24 16:28:59 -0500

    Insertando el footer al index

--- js/router.js
+++ js/router.js
@@ -6,4 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("navbar").innerHTML = data;
         })
         .catch(error => console.error("Error al cargar el header:", error));
+
+    // Cargar el Footer
+    fetch("components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch(error => console.error("Error al cargar el footer:", error));
 });
